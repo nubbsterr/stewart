d734c3fb74aec8cf9b50819225c421035fa59633
Mourad Sherif <sherm5344@gmail.com>
2025-03-01 14:35:25 -0500

    GY521 Commit: Using GY521.h lib over Wire, PID calculation complete and calibration successful. Simplified pinout and removed unneeded variables.

--- SprouthacksProject/SprouthacksProject.ino
+++ SprouthacksProject/SprouthacksProject.ino
@@ -1,63 +1,42 @@
 /* TO-DO:
- * calibrate gy521
- * patch error_read for getAngle, returning -1
- * get pid equation/formula stuff going, conplementaru filter to filter out noise from accelerometer
- * remove I from pid maybe?
- * get angle and acceleration from sensor, angle from I2C lib
+ * calibrate gy521, also set logic for PID # to go back/forwards in loop, get pitch value to find where negative and positive
  * pitch deck for project can be ultra shrimple; show project purpose, design, issues in development, etc.
 */
 
 // Lib include
-#include <Wire.h> // I2C interface for Accelerometer
-#include <math.h> // aTan function for degeree calculation
+#include <GY521.h> // Read MPU6050 data using Wire.h, return angle values for PID calculation
 
-
-// PID 
-const int Kp { 0 };
-const int Ki { 0 };
-const int Kd { 0 };
-volatile int pid_I { 0 };          // Integral value for PID, global to get integral total
-volatile int error { 0 };          // Error = SP - PV, manipulate output to get closer to SP
-volatile int previous_error { 0 }; // Used for Derivative calculation
+/* PID and Accelerometer
+ * INT, SCL, SDA are all handled by GY521.h lib. Pinout is predefined w/ macros in lib header. */
+GY521 mpu(0x68);                        // Create sensor object for GY521 lib interface
+const int Kp { 0 };                     // Proportional-Gain constant for PID
+const int Ki { 0 };                     // Integral-Gain constant for PID
+const int Kd { 0 };                     // Derivative-Gain constant for PID
+int pid_I { 0 };                        // Integral value for PID, global to get integral total
+int error { 0 };                        // Error = SP - PV, manipulate output to get closer to SP
+int previous_error { 0 };               // Used for Derivative calculation
 const float targetAngle { 0 };          // Set Point (SP), aligned upwards against the normal
-volatile float pitchAngle { 0 };        // Process Value (VP), Y-axis angle 
-volatile float yawAngle { 0 };          // X-axis angle
+float pitchAngle { 0 };                 // Process Value (VP), Y-axis angle 
 const float radsToDegrees { 180/M_PI }; // Convert radian measure of accelerometer to degrees
 
-// Accelerometer
-/* INT = interrupt, notifies data is available to read
- * SCL = serial clock, similar to baud rate, synchronize transmission of data
- * SDA = serial data, used for transferring data w/ I2C
-*/ 
-#define MPUADDR 0x68 
-#define SCL 34
-#define SDA 35
-int16_t accel_X { 0 }; // horizonatal acceleration
-int16_t accel_Y { 0 }; // vertical acceleration
-int16_t accel_Z { 0 }; // vertical acceleration
-
+float yawAngle { 0 };   // X-axis angle
 
 // LN298
 #define IN1 26
 #define IN2 27
-#define IN3 14
-#define IN4 12
-#define ENA 33
-#define ENB 25
-volatile int ena_speed { 0 }; // Speed of left-most motor
-volatile int enb_speed { 0 }; // Speed of right-most motor
+#define ENA 25
+int ena_speed { 0 }; // Motor speed
 
 // Timer
-volatile float current_time { 0 };  // Ellapsed time in millisceonds
-volatile float previous_time { 0 };
+float program_time { 0 };  // Ellapsed time in millisceonds from program start
+float previous_time { 0 }; 
+float cycle_time { 0 };    // Cycle time taken to complete PID calculation
 
 void driveStop()
 {
   // all OUT set to Low
   digitalWrite(IN1, LOW); 
   digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
 }
 
 void driveBackward()
@@ -65,8 +44,6 @@ void driveBackward()
   // drive both motors counterclockwise
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
 }
 
 void driveForward()
@@ -74,8 +51,6 @@ void driveForward()
   // drive both motors clockwise
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
 }
 
 void setup() {
@@ -83,23 +58,25 @@ void setup() {
 
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT);
-  pinMode(ENB, OUTPUT);
 
   digitalWrite(ENA, ena_speed);
-  digitalWrite(ENB, enb_speed);
 
-  // Begin I2C communication and wake accelerometer
+  // Start I2C Wire connection to wake MPU then check if MPU is awake before start
   Wire.begin();
-  Wire.beginTransmission(MPUADDR);
-  Wire.write(0x6B); // Wake MPU6050, write to pwr_mgmt register
-  Wire.write(0);    // Send wake signal
-  Wire.endTransmission(true); 
+  if (mpu.wakeup() == false)
+  {
+    Serial.println("MPU is asleep after I2C Start--Angles will not be read from sensor. (0x68 ADDR)");
+  }
+
+  // Set sensor sensitivity and calibrate
+  mpu.setAccelSensitivity(2);    // 8g of force max
+  mpu.setGyroSensitivity(1);     // 500 degreees/second max
+  mpu.setNormalize(true);        // Normalize angle measurements
+  mpu.calibrate(20);             // 20 reads on MPU to calibrate angles to 0
 }
 
-constexpr int calculatePID()
+int calculatePID()
 {
   // Linear models since exponential is too hard/process demanding
 
@@ -113,7 +90,7 @@ constexpr int calculatePID()
 
   /* D controlled by rate of change of error, aims to predict change of process value to correct movement, prevent overshooting.
   Biases output to prevent PV from overshooting SP */
-  int pid_D = Kd*((error-previous_error)/time);
+  int pid_D = Kd*((error-previous_error) / cycle_time);
 
   int pid = pid_P + pid_I + pid_D;
   return pid;
@@ -121,32 +98,26 @@ constexpr int calculatePID()
 
 void getAngle()
 {
-  Wire.write(0x3B); // Write to Accel_X high-byte register
-  Wire.endTransmission(true);
-
-  /* Accelerometer is 16 bits but read only returns 8 bits, so run 2 reads.
-   * First read is shifted 8 units/multiplied by 256, which isolates the top/high 8 bits.
-   * Second read is not shifted and will get the bottom 8 bits.
-   * The two reads are then logically OR'd; added together to get the total acceleration data. 
-  */
-
-  accel_X = Wire.read()<<8 | Wire.read(); 
-  accel_Y = Wire.read()<<8 | Wire.read();
-  accel_Z = Wire.read()<<8 | Wire.read();
+  // Read data from sensor w/ read() then get XYZ-angles, only pitch angle is needed for PID
+  mpu.read();
+  pitchAngle = mpu.getAngleY(); 
+  yawAngle = mpu.getAngleX();
 }
 
 void loop() 
 {
-  previous_time = current_time; // last recorded time
-  current_time = millis();      // millis records time since program start
-  error = targetAngle - 
-  previous_error = error;       
+  previous_time = program_time; // last recorded time
+  program_time = millis();      // millis records time since program start
+  cycle_time = program_time - previous_time; 
+
   getAngle();
+  error = targetAngle - pitchAngle; 
+  previous_error = error;     
 
-  Serial.print("Accel X reading | "); Serial.println(accel_X);
-  Serial.print("Accel Y reading | "); Serial.println(accel_Y);
-  Serial.print("Accel Z reading | "); Serial.println(accel_Z);
 
-  Serial.print("Ellapsed Time (s): "); Serial.println(current_time/1000);
+  Serial.print("Angle Y reading | "); Serial.println(pitchAngle);
+  Serial.print("Angle X reading | "); Serial.println(yawAngle);
+
+  Serial.print("Ellapsed Time (s): "); Serial.println(program_time/1000);
   delay(1000);
 }
